f716204c8ed1b0e3b98f981138dfd44708564a11
kizoo <kizoomac@gmail.com>
2014-11-14 14:11:02 +0900

    wgFileExtensions

--- LocalSettings.php
+++ LocalSettings.php
@@ -190,12 +190,12 @@ $wgLocalFileRepo = array (
 $wgImgAuthPublicTest = false;
 
 // + by Jaewoo
-//$wgFileExtensions[] = 'zip';
-//$wgFileExtensions[] = 'doc';
-//$wgFileExtensions[] = 'xls';
-//$wgFileExtensions[] = 'pdf';
-//$wgFileExtensions[] = 'mp3';
-//$wgFileExtensions[] = 'svg';
+$wgFileExtensions[] = 'zip';
+$wgFileExtensions[] = 'doc';
+$wgFileExtensions[] = 'xls';
+$wgFileExtensions[] = 'pdf';
+$wgFileExtensions[] = 'mp3';
+$wgFileExtensions[] = 'svg';
 
 $wgExternalLinkTarget = '_blank'; // 링크 누르면 새창으로
 $wgAllowExternalImages = true;
